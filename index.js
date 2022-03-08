002d0ab53c438ede921ea939f5675c1fec461c0d
GraceUmutesi <tesigrace98@gmail.com>
2022-03-08 05:34:58 +0200

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(miles){
   // Write your code here!
+
+  if (miles<=400)
+    return('This one is on me!');
+  else if(miles<2500 && miles>2000)
+    return('I will gladly take your thirty bucks.');
+
+  else
+    return('No can do.');
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
   // Write your code here!
+  switch (amount) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
